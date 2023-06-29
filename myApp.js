bd2b4ae6ccd3108cca580daf20e7e166b0b381c3
nivwer <nivwerr@gmail.com>
2023-06-29 15:44:29 -0300

    Solution 3: Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -1,10 +1,21 @@
 let express = require('express');
 let app = express();
 
+// Solution 1:
+// console.log('Hello World')
 
+
+// Solution 2:
+// app.get('/', (req, res) => {
+//    res.send('Hello Express');
+//  });
+
+
+// Solution 3:
 app.get('/', (req, res) => {
-    res.send('Hello Express');
-  });
+  const Path = __dirname + "/views/index.html"
+  res.sendFile(Path);
+});
   
 
 
